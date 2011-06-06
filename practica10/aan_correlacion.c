d127cb2023e32fa4c913364b9d6d8f896a464068
Alberto Ruiz <aruiz@gnome.org>
2011-06-06 03:53:47 +0100

    Media y desviacion estandar de cada pixel de la segunda imagen

--- practica10/aan_correlacion.c
+++ practica10/aan_correlacion.c
@@ -34,11 +34,41 @@ copiar_ventana (float *input,
         output[y * VENTANA + x] = input[(j + y) * height + (i + x)];
 }
 
+float
+media (float *input, int length)
+{
+  int i;
+  float m = 0.0;
+  
+  for (i=0; i < length; i++)
+    m = m + input[i];
+
+  m = m / (float)(length);
+
+  return m;
+}
+
+float
+stddev (float *input, int length, float avg)
+{
+  int i;
+  float sd = 0.0;
+  
+  for (i=0; i < length; i++)
+    sd = sd + (input[i] - avg);
+    
+  sd = sd / (float)length;
+  sd = sqrtf (sd);
+  
+  return sd;
+}
+
+
+
+
 void
 buscar_correlacion (float *vent,
                     float *input,
-                    float  media_input,
-                    float  stddev_input,
                     int    width,
                     int    height,
                     int   *x,
@@ -46,22 +76,26 @@ buscar_correlacion (float *vent,
 {
   float corr;
   int i, j;
+
+  float *area   = (float*)malloc (sizeof (float) * VENTANA * VENTANA);
   
-  float media_ventana = 0.0;
-  
-  for (i=0; i < VENTANA * VENTANA; i++)
-  {
-    media_ventana = media_ventana + vent[i];
-  }
-  media_ventana = media_ventana / (float)(VENTANA * VENTANA);
-  
+  float media_ventana = media (vent, VENTANA * VENTANA);
+  float stddev_ventana = stddev (vent, VENTANA * VENTANA, media_ventana);
+
   for (i = VENTANA / 2; i < (width - VENTANA / 2); i++)
   {
     for (j = VENTANA / 2; j < (height - VENTANA / 2); j++)
     {
+      float sum, media_area, stddev_area;
       
+      copiar_ventana (input, area, width, height, i, j);
+      
+      media_area = media (area, VENTANA * VENTANA);
+      stddev_area = stddev (area, VENTANA * VENTANA, media_area);
     }
   }
+  
+  free (area);
 }
 
 float*
@@ -70,26 +104,12 @@ aan_correlacion (float *a, float *b, int width, int height, int orientacion)
   int i, j;
   float *output = (float*)malloc (sizeof (float) * width * height);
   float *area   = (float*)malloc (sizeof (float) * VENTANA * VENTANA);
-  float  stddev = 0.0;
-  float  media  = 0.0;
 
   for (i=0; i < width * height; i++)
   {
     /* Ponemos todos los valores de la salida a 0 */
     output[i] = 0.0;
-    
-    /* Calculamos la media de la segunda imagen */
-    media = media + b[i];
   }
-  media = media / (float)(width * height);
-  
-  /* Calculamos la desviacion estandar de la segunda imagen */
-  for (i=0; i < width * height; i++)
-  {
-    stddev = stddev + (b[i] + media)*(b[i] + media);
-  }
-  stddev = stddev / (width * height);
-  stddev = sqrtf (stddev);
 
   /* Ignoramos los pixeles de los bordes por no poder llenar una ventana */
   for (i = VENTANA / 2; i < (width - VENTANA / 2); i++)
@@ -98,7 +118,7 @@ aan_correlacion (float *a, float *b, int width, int height, int orientacion)
     {
       int x, y;
       copiar_ventana (a, area, width, height, i, j);
-      buscar_correlacion (area, b, media, stddev, width, height, &x, &y);
+      buscar_correlacion (area, b, width, height, &x, &y);
     }
   }
 
